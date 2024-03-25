fb7eba6c539228b9dd05461a041edafbdee5796c
Koshur1504 <aamir.mir530@gmail.com>
2024-03-25 06:54:06 +0530

    Server Setup

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,21 +1,30 @@
 const jsonServer = require("json-server");
+
+const server = jsonServer.create();
 const auth = require("json-server-auth");
 const cors = require("cors");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
 const middlewares = jsonServer.defaults();
-
 server.db = router.db;
 
-// Set up the authentication middleware
+server.use(middlewares);
+
+
 server.use(cors());
 server.use(auth);
 server.use(middlewares);
 server.use(router);
 
-const port = process.env.PORT || 4000;
 
+const port = process.env.PORT || 4000;
 server.listen(port, () => {
   console.log(`JSON Server with authentication is running on port ${port}`);
 });
 
+
+module.exports = server;
