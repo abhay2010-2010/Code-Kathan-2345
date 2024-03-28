b38181ceeb047f7f59743447e0db7683eea99c88
Zeeshan6871 <furqanspn@gmail.com>
2024-03-28 21:40:21 +0530

    Card3A and Card3B

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
 import "./App.css";
+import { Card3a } from "./components/Card3a/Card3a";
+import { Card3b } from "./components/Card3b/Card3b";
+import Carousel from "./components/CrousalCard/Crousal";
 import { baseUrl } from "./utils/baseUrl";
 
 function App() {
   console.log(baseUrl);
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Card3a />
+      <Card3b />
+      <Carousel />
+    </div>
+  );
 }
 
 export default App;
