8c0a7468957fa74a3b3c0c7b25ed522f14400120
Koshur1504 <aamir.mir530@gmail.com>
2024-03-28 22:38:03 +0530

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import { baseUrl } from "./utils/baseUrl";
 import { Allroutes } from "./routes/Allroutes";
+import { Navbar } from "./components/navbar/Navbar";
 
 function App() {
   console.log(baseUrl);
   return (
     <div className="App">
+      <Navbar />
       <Allroutes />
     </div>
   );
