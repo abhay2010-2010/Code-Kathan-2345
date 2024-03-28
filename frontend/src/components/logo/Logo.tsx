187fccc28ae4ffebcc7857a29dc5e681bf28283c
Koshur1504 <aamir.mir530@gmail.com>
2024-03-29 01:59:11 +0530

    Added Navbar

--- frontend/src/components/logo/Logo.tsx
+++ frontend/src/components/logo/Logo.tsx
@@ -4,7 +4,7 @@ export const Logo = () => (
   <Icon
     viewBox="0 0 2000 1247"
     preserveAspectRatio="xMidYMid meet"
-    width="100%"
+    width="7rem"
     height="auto"
   >
     <svg version="1.0" xmlns="http://www.w3.org/2000/svg">
