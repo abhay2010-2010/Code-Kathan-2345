7cdd3523db83c30567ebb18cc096a7348082ad75
raushanrauss <151566432+raushanrauss@users.noreply.github.com>
2024-03-29 15:48:59 +0530

    Card_Done

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
-import "./index.css";
+
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
