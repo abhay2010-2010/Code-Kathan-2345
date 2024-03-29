f1ff9daf871a78ed06891e141bacbdb26313fc5d
Aamir <Aamir.mir530@gmial.com>
2024-03-29 05:31:55 +0530

    Bugs

--- frontend/src/components/CrousalCard/Crousal.tsx
+++ frontend/src/components/CrousalCard/Crousal.tsx
@@ -1,9 +1,7 @@
-import React from "react";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
 import CardCrousal from "./CardCrousal";
-import { Box } from "@chakra-ui/react";
 
 function Carousel() {
   var settings = {
--- frontend/src/components/logo/Logo.tsx
+++ frontend/src/components/logo/Logo.tsx
@@ -1,4 +1,4 @@
-import { Box, Icon } from "@chakra-ui/react";
+import { Icon } from "@chakra-ui/react";
 
 export const Logo = () => (
   <Icon
