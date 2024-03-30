9b2c77c88592684b2e415ce7d44ea8f388ab218e
Abhay kawle <120048680+abhay2010-2010@users.noreply.github.com>
2024-03-30 23:18:01 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 import Footer from "./components/Footer/Footer";
-import Card2b from "./components/card2b/Card2b";
+
 import { Navbar } from "./components/navbar/Navbar";
 import { Allroutes } from "./routes/Allroutes";
 import { baseUrl } from "./utils/baseUrl";
