629c703b77ca5ae6cb3825c9425893b43937533d
Zeeshan6871 <furqanspn@gmail.com>
2024-03-31 11:57:10 +0530

    News and Sports page added

--- frontend/src/components/Card2a/Card2a.tsx
+++ frontend/src/components/Card2a/Card2a.tsx
@@ -1,5 +1,4 @@
-
-'use client'
+"use client";
 
 import {
   Box,
@@ -9,51 +8,54 @@ import {
   Text,
   Stack,
   Image,
-} from '@chakra-ui/react'
-
+} from "@chakra-ui/react";
 
 const IMAGE =
-  'https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'
+  "https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80";
 
 export default function Card2a() {
   return (
-    <Center py={12} >
+    <Center py={12} width={"full"}>
       <Box
-        role={'group'}
-
-        maxW={'330px'}
-        w={'full'}
-        bg={useColorModeValue('white', 'gray.800')}
-        boxShadow={'2xl'}
-        rounded={'lg'}
-        pos={'relative'}
+        role={"group"}
+        p={6}
+        maxW={"330px"}
+        w={"full"}
+        bg={useColorModeValue("white", "gray.800")}
+        boxShadow={"2xl"}
+        rounded={"lg"}
+        pos={"relative"}
         zIndex={1}
         width={300}
       >
         <Box>
           <Image
-            rounded={'lg'}
-            height={"100%"}
+            rounded={"lg"}
+            height={180}
             width={"100%"}
-            objectFit={'cover'}
+            objectFit={"cover"}
             src={IMAGE}
             alt="#"
           />
-          
         </Box>
-        <Stack align={'start'} textAlign={"start"}>
-          <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500} textDecoration={"underline"} >
-            <span style={{ color: 'red' }}>LIVE</span> 'Crypto King' Sam Bankman-Fried faces lengthy jail term
+        <Stack align={"start"} textAlign={"start"} mt={-25}>
+          <Heading
+            fontSize={"2xl"}
+            fontFamily={"body"}
+            fontWeight={500}
+            textDecoration={"underline"}
+          >
+            <span style={{ color: "red" }}>LIVE</span> 'Crypto King' Sam
+            Bankman-Fried faces lengthy jail term
           </Heading>
-          <Stack direction={'row'} align={'starta'} >
-            <Text color={'gray.500'} fontSize={'sm'}>
-              The former billionaire is about to find out his punishment for massive fraud and money laundering.
+          <Stack direction={"row"} align={"starta"} mt={-35}>
+            <Text color={"gray.500"} fontSize={"sm"}>
+              The former billionaire is about to find out his punishment for
+              massive fraud and money laundering.
             </Text>
           </Stack>
         </Stack>
       </Box>
-
-
     </Center>
-  )
+  );
 }
--- frontend/src/pages/sports/Sports.tsx
+++ frontend/src/pages/sports/Sports.tsx
@@ -1,3 +1,141 @@
+import { Box, Divider, Grid, Heading, Text } from "@chakra-ui/react";
+import Card5b from "../../components/Card5b/Card5b";
+import Card1 from "../../components/Card_1/Card1";
+import { Card3a } from "../../components/Card3a/Card3a";
+import Carousel from "../../components/CrousalCard/Crousal";
+import Card2b from "../../components/card2b/Card2b";
+import TwoCards from "../../components/TwoCards/TwoCards";
+import Card5 from "../../components/Card5/Card5";
+import SearchCard from "../../components/Search Card/SearchCard";
+
 export const Sports = () => {
-  return <div>Sports</div>;
+  return (
+    <>
+      <Heading fontSize={"3rem"} backgroundColor={"#f5d22e"} p={3}>
+        SPORTS
+      </Heading>
+      <Divider backgroundColor={"black"} height={"1px"} />
+      <Box p={5}>
+        <br />
+
+        <br />
+        <Box flexDirection={["column", "row"]} display={"flex"} gap={"2rem"}>
+          <Card5b />
+          <Card1 />
+        </Box>
+        <Carousel />
+        <Grid
+          justifyItems={"center"}
+          templateColumns={["repeat(1,1fr)", "repeat(3,1fr)", "repeat(5,1fr)"]}
+          alignContent={"center"}
+          marginX="auto"
+        >
+          <Box>
+            <Card1 />
+          </Box>
+          <Box>
+            <Card1 />
+          </Box>
+          <Box>
+            <Card1 />
+          </Box>
+          <Box>
+            <Card1 />
+          </Box>
+          <Box>
+            <Card1 />
+          </Box>
+        </Grid>
+
+        <Grid templateColumns={{ sm: "1fr", md: "repeat(3, 1fr)" }} mb={20}>
+          <Card3a />
+          <Card3a />
+          <Card3a />
+        </Grid>
+      </Box>
+      <Box p={5} mt={8}>
+        <br />
+        <Divider backgroundColor={"black"} height={"2px"} />
+        <Text fontSize={"20px"}>Features and analysis</Text>
+        <br />
+        <TwoCards />
+        <Divider backgroundColor={"black"} height={"2px"} mb={5} />
+        <Grid templateColumns={{ sm: "1fr", md: "repeat(3, 1fr)" }}>
+          <Card2b />
+          <Card2b />
+          <Card2b />
+        </Grid>
+      </Box>
+
+      <Box mt={10} p={5}>
+        <Divider backgroundColor={"black"} height={"2px"} />
+        <Heading fontSize={"20px"} mt={5} mb={5}>
+          Technology and Science
+        </Heading>
+        <TwoCards />
+      </Box>
+
+      <Box p={5}>
+        <Divider backgroundColor={"black"} height={"2px"} />
+        <Heading fontSize={"20px"} mt={5} mb={5}>
+          Executive
+        </Heading>
+        <Card5 />
+        <Divider backgroundColor={"black"} height={"2px"} mt={5} />
+        <Heading fontSize={"20px"} mt={5} mb={5}>
+          More news and features
+        </Heading>
+        <Grid
+          justifyItems={"center"}
+          templateColumns={["repeat(1,1fr)", "repeat(3,1fr)", "repeat(4,1fr)"]}
+          alignContent={"center"}
+          marginX="auto"
+          mt={10}
+        >
+          <Box>
+            <Card1 />
+          </Box>
+          <Box>
+            <Card1 />
+          </Box>
+          <Box>
+            <Card1 />
+          </Box>
+          <Box>
+            <Card1 />
+          </Box>
+          <Box>
+            <Card1 />
+          </Box>
+          <Box>
+            <Card1 />
+          </Box>
+          <Box>
+            <Card1 />
+          </Box>
+          <Box>
+            <Card1 />
+          </Box>
+        </Grid>
+      </Box>
+
+      <Carousel />
+
+      <Box p={10}>
+        <Divider backgroundColor={"black"} height={"2px"} />
+        <Heading fontSize={"20px"} mt={5} mb={5}>
+          Also in news
+        </Heading>
+        <SearchCard />
+        <SearchCard />
+        <SearchCard />
+        <SearchCard />
+        <SearchCard />
+        <SearchCard />
+        <SearchCard />
+        <SearchCard />
+        <SearchCard />
+      </Box>
+    </>
+  );
 };
