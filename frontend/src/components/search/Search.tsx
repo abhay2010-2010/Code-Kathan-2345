8c1de81293acd63b0acf65e4aeaecfcd5091291d
Zeeshan Ahmad <furqanspn@gmail.com>
2024-04-04 02:22:13 +0530

    Update Search.tsx

--- frontend/src/components/search/Search.tsx
+++ frontend/src/components/search/Search.tsx
@@ -1,7 +1,5 @@
-import { Button, Input, InputGroup } from "@chakra-ui/react";
-import { useState } from "react";
+import { Input} from "@chakra-ui/react";
 import { CiSearch } from "react-icons/ci";
-import { useNavigate } from "react-router-dom";
 
 export const Search = () => {
 
