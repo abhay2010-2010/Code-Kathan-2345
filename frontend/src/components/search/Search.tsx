7370a32be87b05718948129693856d21f5a28858
Zeeshan6871 <furqanspn@gmail.com>
2024-04-03 13:09:53 +0530

    aticle

--- frontend/src/components/search/Search.tsx
+++ frontend/src/components/search/Search.tsx
@@ -1,13 +1,66 @@
 import { Input, InputGroup, InputRightElement } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
 import { CiSearch } from "react-icons/ci";
+import { useSearchParams } from "react-router-dom";
+import { useDebounce } from "../../hooks/Debounce";
+import { useData } from "../../utils/dataContext/dataContext";
+
+interface queryParams {
+  q?: string;
+  _limit?: number;
+  _page?: number;
+}
 
 export const Search = () => {
+  const { getPosts, totalPosts } = useData();
+  const [params, setParams] = useSearchParams();
+  const [query, setQuery] = useState(params.get("q") || "");
+
+  const debouncedQuery = useDebounce(query, 1000);
+
+  const setUrl = () => {
+    setParams((prev) => {
+      let obj: queryParams = {};
+
+      const searchParams = new URLSearchParams(prev.toString());
+      for (const [key, value] of searchParams.entries()) {
+        obj[key] = value;
+      }
+
+      if (query) {
+        obj.q = query;
+      } else {
+        delete obj.q;
+      }
+      return obj;
+    });
+  };
+
+  useEffect(() => {
+    setUrl();
+    getPosts();
+  }, [totalPosts, debouncedQuery]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setQuery(value);
+  };
+
+  // console.log(posts, baseUrl, totalPosts);
+
   return (
-    <InputGroup>
-      <InputRightElement pointerEvents="none">
-        <CiSearch size={"2rem"} />
-      </InputRightElement>
-      <Input type="text" placeholder="Search" />
-    </InputGroup>
+    <>
+      <InputGroup>
+        <InputRightElement pointerEvents="none">
+          <CiSearch size={"2rem"} />
+        </InputRightElement>
+        <Input
+          type="text"
+          placeholder="Search"
+          value={query}
+          onChange={handleChange}
+        />
+      </InputGroup>
+    </>
   );
 };
--- frontend/src/pages/Article/Article.tsx
+++ frontend/src/pages/Article/Article.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
+  Flex,
+  useToast,
   Table,
   Thead,
   Tbody,
@@ -14,65 +16,130 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
-  useDisclosure,
-  Box,
   Input,
-  Textarea,
+  ButtonGroup,
+  Text,
+  Box,
+  useDisclosure,
 } from "@chakra-ui/react";
 import SidebarWithHeader from "../../components/AdminNavBar/AdminNavbar";
+import { Search } from "../../components/search/Search";
+import { useSearchParams } from "react-router-dom";
+import { useData } from "../../utils/dataContext/dataContext";
+import { IPatchArticle } from "../../utils/dataContext/types";
 
-interface Article {
-  title: string;
-  Description: string;
-  source: string;
-  category: string;
-  articleLink: string;
+interface queryParams {
+  q?: string;
+  _page?: number;
+  _limit?: number;
 }
 
-interface TableProps {
-  data: Article[];
-}
+let data: IPatchArticle = {
+  id: 1,
+  title: "daniel Kahneman, Nobel Prize-winning psychologist, 1934-2024",
+  Description:
+    'Three people have been released, but the situation "is not over yet", police in the city of Ede say.',
+  source: "BBC.com",
+  time: 4,
+  articleLink: "https://www.bbc.com/news/world-europe-68698022",
+  image1:
+    "https://news.google.com/api/attachments/CC8iK0NnNWhiUzF6VjBrelprMTJUalJIVFJDZkF4ampCU2dLTWdZVk1KUnBHUWs=-w280-h168-p-df-rw",
+  image2:
+    "https://i.zedtranslate.com/newsimage/CC8iK0NnNWhiUzF6VjBrelprMTJUalJIVFJDZkF4ampCU2dLTWdZVk1KUnBHUWs",
+  category: "world",
+  clicks: 283,
+};
 
-const data = [
-  {
-    title: "daniel Kahneman, Nobel Prize-winning psychologist, 1934-2024",
-    Description:
-      'Three people have been released, but the situation "is not over yet", police in the city of Ede say.',
-    source: "BBC.com",
-    time: 4,
-    articleLink: "https://www.bbc.com/news/world-europe-68698022",
-    image1:
-      "https://news.google.com/api/attachments/CC8iK0NnNWhiUzF6VjBrelprMTJUalJIVFJDZkF4ampCU2dLTWdZVk1KUnBHUWs=-w280-h168-p-df-rw",
-    image2:
-      "https://i.zedtranslate.com/newsimage/CC8iK0NnNWhiUzF6VjBrelprMTJUalJIVFJDZkF4ampCU2dLTWdZVk1KUnBHUWs",
-    category: "world",
-  },
-  // Add more data items as needed
-];
-
-const Article: React.FC<TableProps> = () => {
+const Article: React.FC = () => {
+  const { posts, getPosts, totalPosts, patchPost, deletePost } = useData();
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [selectedItem, setSelectedItem] = useState<Article | null>(null);
+  const [params, setParams] = useSearchParams();
+  const [selectedItem, setSelectedItem] = useState<IPatchArticle>(data);
+  const [page, setPage] = useState<number>(Number(params.get("_page") || "1"));
+  const [limit, Setlimit] = useState<number>(
+    Number(params.get("_limit") || "10"),
+  );
+  const [pages, setpages] = useState(1);
+  const [updateItem, setupdateItem] = useState<IPatchArticle>(selectedItem);
+  const [addNewsModalOpen, setAddNewsModalOpen] = useState(false);
+  const toast = useToast();
 
-  const handleDelete = (index: number) => {
-    // Implement delete logic here
-    console.log("Deleting item at index:", index);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setupdateItem({ ...updateItem, [name]: value });
   };
 
-  const handleUpdate = (item: Article) => {
+  const setUrl = () => {
+    setParams((prev) => {
+      let obj: queryParams = { _limit: limit, _page: page };
+      return obj;
+    });
+  };
+
+  useEffect(() => {
+    setpages(Math.ceil(totalPosts || 0) / Number(limit));
+    setUrl();
+    setTimeout(() => {
+      getPosts(), 500;
+    });
+  }, [page, limit]);
+
+  const handleDelete = (id: number) => {
+    let examplePromise = deletePost(id);
+    toast.promise(examplePromise, {
+      success: { title: "News removed", description: "Removed Successfull" },
+      error: { title: "Somthing Went Wrong", description: "Something wrong" },
+      loading: { title: "Deleting News", description: "Please wait" },
+    });
+    getPosts();
+    console.log("Deleting item at index:", id);
+  };
+
+  const handleOpenUpdate = (item: IPatchArticle) => {
+    console.log(item);
     setSelectedItem(item);
     onOpen();
   };
 
-  const handleCloseModal = () => {
-    setSelectedItem(null);
+  const handleUpdate = () => {
+    let examplePromise = patchPost(updateItem);
+    toast.promise(examplePromise, {
+      success: { title: "Update Successfull", description: "News Updated" },
+      error: { title: "Somthing Went Wrong", description: "Something wrong" },
+      loading: { title: "Updating News", description: "Please wait" },
+    });
+    getPosts();
     onClose();
+  };
+
+  const handleCloseModal = () => {
+    setSelectedItem(data);
+    setupdateItem(data);
+    onClose();
+  };
+
+  const handleAddNewsOpen = () => {
+    setAddNewsModalOpen(true);
+  };
+
+  const handleAddNewsClose = () => {
+    setAddNewsModalOpen(false);
+  };
+
+  const handleAddNews = () => {
+    // Logic to add news
   };
 
   return (
     <>
       <SidebarWithHeader />
       <Box h={"100vh"} ml={{ base: 0, md: 60 }} p="4" textAlign={"center"}>
+        <Flex gap={5}>
+          <Search />
+          <Button colorScheme="green" onClick={handleAddNewsOpen}>
+            Add News
+          </Button>
+        </Flex>
         <Table variant="simple">
           <Thead>
             <Tr>
@@ -80,34 +147,53 @@ const Article: React.FC<TableProps> = () => {
               <Th>Description</Th>
               <Th>Source</Th>
               <Th>Category</Th>
-              <Th colSpan={2}>Actions</Th>
+              <Th colSpan={2}>Actions Button</Th>
             </Tr>
           </Thead>
           <Tbody>
-            {data.map((item, index) => (
-              <Tr key={index}>
-                <Td>{item.title}</Td>
-                <Td>{item.Description}</Td>
-                <Td>{item.source}</Td>
-                <Td>{item.category}</Td>
-                <Td>
-                  <Button colorScheme="blue" onClick={() => handleUpdate(item)}>
-                    Update
-                  </Button>
-                </Td>
-                <Td>
-                  <Button
-                    colorScheme="red"
-                    ml={2}
-                    onClick={() => handleDelete(index)}
-                  >
-                    Delete
-                  </Button>
-                </Td>
-              </Tr>
-            ))}
+            {posts.length != 0 &&
+              posts.map((item, index) => (
+                <Tr key={index}>
+                  <Td>{item.title}</Td>
+                  <Td>{item.Description}</Td>
+                  <Td>{item.source}</Td>
+                  <Td>{item.category}</Td>
+                  <Td>
+                    <Button
+                      colorScheme="blue"
+                      onClick={() => handleOpenUpdate(item)}
+                    >
+                      Update
+                    </Button>
+                  </Td>
+                  <Td>
+                    <Button
+                      colorScheme="red"
+                      ml={2}
+                      onClick={() => handleDelete(item.id)}
+                    >
+                      Delete
+                    </Button>
+                  </Td>
+                </Tr>
+              ))}
           </Tbody>
         </Table>
+        <ButtonGroup>
+          <Button
+            isDisabled={page <= 1}
+            onClick={() => setPage((prev) => prev - 1)}
+          >
+            Previous
+          </Button>
+          <Text>{page}</Text>
+          <Button
+            isDisabled={page === pages}
+            onClick={() => setPage((prev) => prev + 1)}
+          >
+            Next
+          </Button>
+        </ButtonGroup>
 
         {/* Modal for Update */}
         <Modal isOpen={isOpen} onClose={handleCloseModal}>
@@ -118,18 +204,64 @@ const Article: React.FC<TableProps> = () => {
             <ModalBody>
               {/* Add form fields for updating item */}
               {/* For example: */}
-              <Input type="text" defaultValue={selectedItem?.title} />
-              <Textarea defaultValue={selectedItem?.Description} />
-              <Input type="text" defaultValue={selectedItem?.source} />
-              <Input type="text" defaultValue={selectedItem?.category} />
-              <Input type="text" defaultValue={selectedItem?.articleLink} />
-              {/* Add other form fields as needed */}
+              <Input
+                type="text"
+                defaultValue={selectedItem?.title}
+                name="title"
+                onChange={handleChange}
+              />
+              <Input
+                type="text"
+                defaultValue={selectedItem?.Description}
+                name="Description"
+                onChange={handleChange}
+              />
+              <Input
+                type="text"
+                defaultValue={selectedItem?.source}
+                name="source"
+                onChange={handleChange}
+              />
+              <Input
+                type="text"
+                defaultValue={selectedItem?.category}
+                name="category"
+                onChange={handleChange}
+              />
+              <Input
+                type="text"
+                defaultValue={selectedItem?.artilcleLink}
+                name="articleLink"
+                onChange={handleChange}
+              />
             </ModalBody>
             <ModalFooter>
-              <Button colorScheme="blue" mr={3}>
+              <Button colorScheme="blue" mr={3} onClick={handleUpdate}>
                 Update
               </Button>
               <Button onClick={handleCloseModal}>Cancel</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+
+        {/* Add News Modal */}
+        <Modal isOpen={addNewsModalOpen} onClose={handleAddNewsClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Add News</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              {/* Add form fields for adding news */}
+              {/* For example: */}
+              <Input type="text" placeholder="Title" name="title" />
+              <Input type="text" placeholder="Description" name="description" />
+              {/* Add other form fields as needed */}
+            </ModalBody>
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3} onClick={handleAddNews}>
+                Add News
+              </Button>
+              <Button onClick={handleAddNewsClose}>Cancel</Button>
             </ModalFooter>
           </ModalContent>
         </Modal>
